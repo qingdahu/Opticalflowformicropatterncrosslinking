da68019bbafa89be397b2c7be73eab80aff48acc
qingdahu <dada_hu30@hotmail.com>
2021-05-25 11:45:54 -0700

    changed the approach for visualization
    now it shows what spherical particles will look like due to the deformation

---
+++ strainopticalflow2.m
@@ -0,0 +1,337 @@
+clear
+close all
+
+% updated 2021/5/20 
+% Written by Qingda Hu
+% Based on the formulas from
+% https://www.files.ethz.ch/structuralgeology/sms/NumModRocDef/Strain_Tensors.pdf
+% and https://ocw.mit.edu/courses/mechanical-engineering/2-080j-structural-mechanics-fall-2013/course-notes/MIT2_080JF13_Lecture2.pdf
+
+ 
+%% preprocessing of data
+% we want to load the the Ux and Uy (displacement, in units of pixels)
+% we also want to keep track of the dx (in units of pixels)
+
+%if you want to just load one: choose from all the .mat files that have
+%flowAll objects in them
+load('circle3.tif.mat')     %load .mat with flowAll object
+%%
+Ux = flowAll{2}.Vx;         % load Ux between first and second frame 
+Uy = flowAll{2}.Vy;         % load Uy between first and second frame 
+for n=3:21                  % loop to 21 if you want 500 scans, 81 if you want 2000 scans (where it exists)
+Ux = Ux+ flowAll{n}.Vx;     % collect all the Ux 
+Uy = Uy+ flowAll{n}.Vy;
+end
+
+
+% % we may want a more complicated processing process involving some
+% % complicated filtering to account for the poresize of the fiber network 
+% Ux = zeros(size(flowAll{2}.Vx));         
+% Uy = zeros(size(flowAll{2}.Vx));         
+% for n=2:21                  % loop to 21 if you want 500 scans, 81 if you want 2000 scans (where it exists)
+% Ux = Ux+ ordfilt2( flowAll{n}.Vx, 6, true(3));     % collect all the Ux 
+% Uy = Uy+ ordfilt2( flowAll{n}.Vy, 6, true(3));
+% end
+
+
+
+%if you want to remove drift
+Ux= Ux - mean(Ux(:)); % global drift removal in x
+Uy= Uy - mean(Uy(:)); % global drift removal in y
+% you may want to consider local drift remove vs global
+
+%you may want to smooth or resize the images
+scale = 0.01;
+Ux = imresize( imgaussfilt(  Ux, 1/scale), scale,'bicubic');
+Uy = imresize( imgaussfilt(  Uy, 1/scale), scale,'bicubic');
+
+% if the image has been scaled down, we need to account for that. If not, h needs to be 1 or else code will error 
+try
+h = 1/scale;
+catch 
+    h=1;
+end
+
+
+%if you want to visualize Ux and Uy at this stage before moving forward
+figure
+x = ((1:size(Ux,1)) + round(1/2))*h; %set up the grid for where the arrows go
+y = ((1:size(Ux,2)) + round(1/2))*h;
+q =quiver(x,y,Ux,Uy,0); %draw arrows, arrows are exact length and not scaled by any factors if scale=0[var5]. otherwise they are scaled.
+q.Color = 'black';
+set(gca,'Ydir','reverse')
+axis equal
+axis off
+set(gca,'color','none')
+figure
+imagesc( sqrt(Ux.^2 + Uy.^2) /7.5758)
+colorbar
+
+
+%% Calculating displacement gradient tensor
+clear estrain wrotation Vs Ds  % clearning these variable helps to avoid errors due to changes in scaling between runs
+
+
+%test case 
+% Ux = repmat([1:16], [16  1]);
+% Uy = zeros([16 16]);
+
+[Uxx,Uxy] = gradient(Ux,h,h); % and we are done!
+[Uyx,Uyy] = gradient(Uy,h,h);
+
+
+
+% We may want to rearrange H so that we can call the nummbers easier
+for i=1:size(Ux,1)
+    for j=1:size(Ux,2)
+        Hdisplacementgradient(i,j,:,:) =  [Uxx(i,j) Uxy(i,j); Uyx(i,j) Uyy(i,j)];
+    end
+end
+
+%%
+% % if we want engineering strain for small deformations
+% for i=1:size(Ux,1)
+%     for j=1:size(Ux,2)
+%         estrain(i,j,:,:) = (squeeze(Hdisplacementgradient(i,j,:,:)) + squeeze(Hdisplacementgradient(i,j,:,:))')./2 ;      % strain
+%         wrotation(i,j,:,:)  =(squeeze(Hdisplacementgradient(i,j,:,:)) - squeeze(Hdisplacementgradient(i,j,:,:))')./2  ;   % rotation
+%         [V,D] = eig(squeeze(estrain(i,j,:,:)));                                      % V are the eigenvectors and D are the eigenvalues
+%         Vs(i,j,:,:) = V;                                                    % I think its [x1,x2;y1,y2] (i think)
+%         Ds(i,j,:,:) = D;
+%     end
+% end        
+% 
+% %if you want Cauchy-Green
+% for i=1:size(Ux,1)
+%     for j=1:size(Ux,2)
+%         F = squeeze(Hdisplacementgradient(i,j,:,:)) + eye(2);
+%         estrain(i,j,:,:) = F'*F ;      % Cauchy-Green strain tensor
+%     end
+% end      
+
+
+%% We actually need to go about finding the change in anisotropy due to the displacement (with affine displacement assumption)
+% can we directly find the change in anisotropy without going to strain and
+% is that faster/better or not?
+% can we just insert text vectors and ask how they are transformed based on
+% matrix F?
+
+figure
+imagesc( sqrt(Ux.^2 + Uy.^2) /7.5758)
+hold on
+
+
+for i=1:size(Ux,1)
+    for j=1:size(Ux,2)
+        F = squeeze(Hdisplacementgradient(i,j,:,:)) + eye(2);
+        alignmentdistribution = [1:360];
+        slicesize = alignmentdistribution(2)-alignmentdistribution(1);
+        circlesize = 0.4;
+        numofslices = ceil(36/2);
+        
+        testvectors = [cosd(alignmentdistribution)', sind(alignmentdistribution)'];
+        testvectorsprime = testvectors * F;
+        alignmentdistribution = atand(testvectorsprime(:,2)./testvectorsprime(:,1));
+
+        for slice = 1:numofslices
+            anglemax = 180/numofslices * ( slice - 0.5 );
+            anglemin = 180/numofslices * ( slice - 1.5 );
+
+            anglecount = sum(   ( alignmentdistribution<anglemax &  alignmentdistribution>anglemin ) | ( alignmentdistribution<(anglemax-180) &  alignmentdistribution>(anglemin-180) )    );
+
+
+            patchx = [ i,i+circlesize*cosd(anglemax),i+circlesize*cosd(anglemin)];
+            patchy = [ j,j-circlesize*sind(anglemax),j-circlesize*sind(anglemin)];
+            color = [0.5 , 0.5 , 0.5]* ((anglecount / (length(alignmentdistribution)/numofslices) -1)*4 +1       ) ;
+            if anglecount>(length(alignmentdistribution)/numofslices)*1.2
+                color = [1 1 1];
+            elseif anglecount<(length(alignmentdistribution)/numofslices)*0.8
+                color = [0 0 0];
+            end
+            patch(patchx,patchy, color , 'EdgeColor', 'none')
+            
+            patchx = [ i,i-circlesize*cosd(anglemax),i-circlesize*cosd(anglemin)];
+            patchy = [ j,j+circlesize*sind(anglemax),j+circlesize*sind(anglemin)];
+            patch(patchx,patchy, color , 'EdgeColor', 'none')
+            
+        end
+        
+        
+        
+        
+        
+    end
+end    
+
+hold off
+axis equal tight
+
+
+%% alternative 1
+
+figure
+imagesc( sqrt(Ux.^2 + Uy.^2) /7.5758)
+hold on
+
+
+for i=1:size(Ux,1)
+    for j=1:size(Ux,2)
+        F = squeeze(Hdisplacementgradient(i,j,:,:)) + (squeeze(Hdisplacementgradient(i,j,:,:))+squeeze(Hdisplacementgradient(i,j,:,:))')*5 + eye(2);                                         % do we only want to scale up the stretching and not the rotation?
+        alignmentdistribution = [1:360];
+        slicesize = alignmentdistribution(2)-alignmentdistribution(1);
+        circlesize = 0.3;
+        numofslices = ceil(36/2);
+        
+        testvectors = [cosd(alignmentdistribution)', sind(alignmentdistribution)'];
+        testvectorsprime = testvectors.*circlesize * F;
+        testvectorsprimex = i + testvectorsprime(:,1);
+        testvectorsprimey = j - testvectorsprime(:,2);
+        fill(testvectorsprimex,testvectorsprimey,[0.5 0.5 0.5])
+        
+        
+        
+        
+    end
+end    
+
+hold off
+axis equal tight
+
+
+
+% figure
+% pcolor(peaks)
+% colorbar
+% axis square
+%%
+% 
+% %Using engineering strain (code is probably correct but need more testing)
+% alignmentdistributions = [];
+% for i=1:size(Vs,1)
+%     for j=1:size(Vs,2)
+%         alignmentdistribution = [0.1:0.1:360];
+%         alignmentdistribution = atand(   (sind(alignmentdistribution)*(1+Ds(i,j,1,1)) )  ./   (cosd(alignmentdistribution)*(1+Ds(i,j,2,2)) )); 
+%         alignmentdistribution = alignmentdistribution + atand(Vs(i,j,1,1)/Vs(i,j,2,1));
+%         alignmentdistribution(alignmentdistribution<-90) = alignmentdistribution(alignmentdistribution<-90) + 180;
+%         alignmentdistribution(alignmentdistribution>90) = alignmentdistribution(alignmentdistribution>90) - 180;
+%         alignmentdistributions(i,j,:) = alignmentdistribution;
+%     end
+% end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% old code from here down
+
+% figure
+% %quiver(squeeze(estrain(:,:,1,1)),squeeze((estrain(:,:,2,2)))) ;
+% subplot(2,2,1)
+% imagesc(estrain(:,:,1,1))
+% title('e11')
+% axis equal tight
+% colorbar
+% subplot(2,2,2)
+% imagesc(estrain(:,:,1,2))
+% title('e12')
+% axis equal tight
+% colorbar
+% subplot(2,2,3)
+% imagesc(estrain(:,:,2,1))
+% title('e21')
+% axis equal tight
+% colorbar
+% subplot(2,2,4)
+% imagesc(estrain(:,:,2,2))
+% title('e22')
+% axis equal tight
+% colorbar
+% 
+% 
+% figure
+% subplot(2,2,1)
+% imagesc(wrotation(:,:,1,1))
+% title('w11')
+% axis equal tight
+% colorbar
+% subplot(2,2,2)
+% imagesc(wrotation(:,:,1,2))
+% title('w12')
+% axis equal tight
+% colorbar
+% subplot(2,2,3)
+% imagesc(wrotation(:,:,2,1))
+% title('w21')
+% axis equal tight
+% colorbar
+% subplot(2,2,4)
+% imagesc(wrotation(:,:,2,2))
+% title('w22')
+% axis equal tight
+% colorbar
+% 
+% figure 
+% quiver(Vs(:,:,1,1).*Ds(:,:,1,1),Vs(:,:,2,1).*Ds(:,:,1,1),1)
+% hold on
+% quiver(Vs(:,:,1,2).*Ds(:,:,2,2),Vs(:,:,2,2).*Ds(:,:,2,2),1)
+% hold off
+% axis equal tight
+% 
+% 
+% 
+% 
+% figure
+% subplot(2,2,1)
+% imagesc(Ds(:,:,1,1))
+% title('\lambda1')
+% axis equal tight
+% colorbar
+% subplot(2,2,2)
+% imagesc(Ds(:,:,2,2))
+% title('\lambda2')
+% axis equal tight
+% colorbar
+% 
+% 
+% 
+% % 
+% % figure 
+% % quiver(Vs(3:end-2,3:end-2,1,2).*Ds(3:end-2,3:end-2,1,1)+ Vs(3:end-2,3:end-2,1,2).*Ds(3:end-2,3:end-2,2,2)   , Vs(3:end-2,3:end-2,2,2).*Ds(3:end-2,3:end-2,1,1)+ Vs(3:end-2,3:end-2,2,2).*Ds(3:end-2,3:end-2,2,2)  ,1)
+% %%
+% 
+% 
+% %% first attempt at visualization
+% 
+% figure
+% histogram(alignmentdistributions(:))
+% 
+% temp1 = alignmentdistributions(25:32,2:10,:);
+% figure
+% subplot(2,2,1)
+% histogram(temp1(:) )
+% temp1 = alignmentdistributions(12:17, 12:17,:);
+% subplot(2,2,2)
+% histogram(temp1(:) )
+% temp1 = alignmentdistributions(2:8, 2:8,:);
+% subplot(2,2,3)
+% histogram(temp1(:) )
+% temp1 = alignmentdistributions(20:25, 20:25,:);
+% subplot(2,2,4)
+% histogram(temp1(:) )
+% 
+% 
+% temp1 = alignmentdistributions([10:20,60:70],20:60,:);
+% figure
+% subplot(2,2,1)
+% histogram(temp1(:) )
+% temp1 = alignmentdistributions(30:50, 20:60,:);
+% subplot(2,2,2)
+% histogram( temp1(:))
